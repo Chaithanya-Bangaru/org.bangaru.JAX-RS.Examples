c91332ad648429dd8467f41bbefc4a43a1690b4c
Chaithanya <chaithanya.bangaru@gmail.com>
2013-06-03 00:31:09 +0530

    Revert "Revert "Revert "Revert "MySQL DS creation from Weblogic and MYSQK database connection test""""
    This reverts commit 204fb52737954df9c0148f6634e9897746215428.

--- src/org/bangaru/ws/rest/BasicRESTAnnotation.java
+++ src/org/bangaru/ws/rest/BasicRESTAnnotation.java
@@ -1,22 +1,53 @@
 package org.bangaru.ws.rest;
 
-import javax.ws.rs.*;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-@Path(value = "/rs/webservice")
+import org.banaru.mysql.dao.JavaRestJNDI;
+
+@Path(value = "/webservice")
 public class BasicRESTAnnotation {
 	private static final String jerseyApiVersion = "1.17";
-	
+
 	@GET
 	@Produces(MediaType.TEXT_HTML)
 	public String returnTitle(){
 		return "<h3>Java RESTful WebService<h3>";
 	}
-	
+
 	@GET
-	@Path(value = "jersey/version")
+	@Path(value = "/jersey/version")
 	@Produces(MediaType.TEXT_HTML)
 	public String returnAPIVersion(){
 		return "<h1>Java RESTful Jersey version used is :<h1> "+jerseyApiVersion;
+	}
+
+	@GET
+	@Produces(MediaType.TEXT_HTML)
+	@Path(value = "/dbTime")
+	public String getDateTimeinMySqlDatabase(){
+		PreparedStatement pst = null;
+		ResultSet rs = null;
+		String myString = null;
+		String dbReturnString = null;
+
+		try {
+			pst = JavaRestJNDI.getConnectionFromMySqlDS().getConnection().prepareStatement("SELECT curdate(),curtime() as DATETIME");
+			rs = pst.executeQuery();
+
+			while(rs.next()){
+				myString = rs.getString("DATETIME");
+			}
+			pst.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		dbReturnString = "<h1>Current Time at DB end is :<h1> "+myString;
+		return dbReturnString;
 	}
 }
